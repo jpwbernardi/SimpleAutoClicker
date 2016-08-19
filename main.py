1d4b80c8a5ce3d6260e5b71610f162371a1b5006
João Winckler <jpwbernardi@hotmail.com>
2016-08-19 00:42:25 -0300

    WIP

--- main.py
+++ main.py
@@ -3,6 +3,23 @@
 import tkinter as tk
 import pyautogui
 import threading
+from pynput.keyboard import Key, Listener
+
+def on_press(key):
+    print('{0} pressed'.format(
+        key))
+    if (key == Key.f8):
+        print('F8!')
+        say_hi()
+    elif (key == Key.f9):
+        print('F9!')
+        say_bye()
+
+def on_release(key):
+    print('{0} release'.format(
+        key))
+    if key == Key.esc:
+        return False
 
 class thread(threading.Thread):
     def __init__(self):
@@ -16,8 +33,26 @@ class thread(threading.Thread):
         return self._stopper.is_set()
 
     def run(self):
+        self._stopper = threading.Event()
         while (self._stopper.is_set() == False):
             pyautogui.click()
+
+class kbthread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self._stopper = threading.Event()
+
+    def stopit(self):
+        self._stopper.set()
+
+    def stopped(self):
+        return self._stopper.is_set()
+
+    def run(self):
+        with Listener(
+                on_press=on_press,
+                on_release=on_release) as self.listener:
+            self.listener.join()
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -28,28 +63,32 @@ class Application(tk.Frame):
     def create_widgets(self):
         self.start = tk.Button(self)
         self.start["text"] = "Start(F8)"
-        self.start["command"] = self.say_hi
+        self.start["command"] = say_hi
         self.start.pack(side="top")
 
         self.stop = tk.Button(self)
         self.stop["text"] = "Stop(F9)"
-        self.stop["command"] = self.say_bye
+        self.stop["command"] = say_bye
         self.stop.pack(side="top")
 
-    def say_hi(self):
-        if (t.isAlive() == False):
-            t.start()
+def say_hi():
+    if (t.isAlive() == False):
+        t.start()
 
-    def say_bye(self):
-        if (t.isAlive() == True and t.stopped() == False):
-            t.stopit()
-            t.join()
+def say_bye():
+    global t
+    if (t.isAlive() == True and t.stopped() == False):
+        t.stopit()
+        t.join()
+        t = thread()
 
 def on_closing():
+    kb.listener.stop()
     root.destroy()
 
 t = thread()
-
+kb = kbthread()
+kb.start()
 root = tk.Tk()
 app = Application(master=root)
 
